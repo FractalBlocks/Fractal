d0ec5586c4b590a4bb5c2881de77090fd7eacde0
Carlos Galarza <carloslfu@gmail.com>
2016-10-14 14:24:18 -0500

    Add Stream spec (Fixes prior commit)

--- src/stream.ts
+++ src/stream.ts
@@ -1,10 +1,52 @@
 
 export interface Stream<T> {
-  subscriptions: [Subcription<T>]
-  subscribe(subscription: Subcription<T>): boolean
+  value(val: T | undefined): T | undefined
+  subscriptions: [Subcription<T>] | undefined[]
+  subscribe(subscription: Subcription<T>): void
   unsubscribe(subscription: Subcription<T>): boolean
 }
 
 export interface Subcription<T> {
   (value: T): void
+}
+
+export function newStream<T>(initialValue: T | undefined): Stream<T> {
+  let subscriptions = []
+  let state = {
+    value: initialValue
+  }
+  let stateProxy = new Proxy<any>(state, {
+    set(target, name, value) {
+      target[name] = value
+      notify(value)
+      return true
+    },
+  })
+  function notify(value) {
+    for(let i = 0, subs; subs = subscriptions[i]; i++) {
+      subs(value)
+    }
+  }
+  return {
+    value(val) {
+      if (val !== undefined) {
+        stateProxy.value = val
+      } else {
+        return state.value
+      }
+    },
+    subscriptions,
+    unsubscribe(subscription) {
+      let index = subscriptions.indexOf(subscription)
+      if (index != -1) {
+        subscriptions.splice(index, 1)
+        return true
+      } else {
+        return false
+      }
+    },
+    subscribe(subscription) {
+      subscriptions.push(subscription)
+    },
+  }
 }
---
+++ tests/stream.spec.ts
@@ -0,0 +1,28 @@
+import { Stream, newStream } from '../src/stream'
+
+describe('Stream spec', function() {
+  let stream$: Stream<number>
+  it('Should return initial value', function() {
+    stream$ = newStream<number>(12)
+    expect(stream$.value(undefined)).toBe(12)
+  })
+  it('Should change the value', function() {
+    stream$.value(102)
+    expect(stream$.value(undefined)).toBe(102)
+  })
+  let sideEffect: number | undefined = undefined
+  let subscriber = function(value: number) {
+    sideEffect = value
+  }
+  it('Should subscribe a function', function() {
+    stream$.subscribe(subscriber)
+    stream$.value(234)
+    expect(sideEffect).toBe(234)
+  })
+  it('Should unsubscribe a function', function() {
+    sideEffect = undefined
+    stream$.unsubscribe(subscriber)
+    stream$.value(401)
+    expect(sideEffect).toBe(undefined)
+  })
+})
