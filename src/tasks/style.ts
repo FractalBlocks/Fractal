92ec20b3cd507b2ad6a9aa9275eec35ebb712255
Carlos Galarza <carloslfu@gmail.com>
2017-02-20 22:19:10 -0500

    Working on style task for worker support

---
+++ src/tasks/style.ts
@@ -0,0 +1,9 @@
+// insert styles in a DOM container at head
+
+import { TaskHandler } from '../index'
+
+export const style: TaskHandler = (containerName: string) => mod => {
+  return function (value) {
+
+  }
+}
