e582a937925948e08cb49d0be05e8719e4f8f502
Carlos Galarza <carloslfu@gmail.com>
2017-03-16 11:40:48 -0500

    Update compose example

--- src/examples/compose/index.ts
+++ src/examples/compose/index.ts
@@ -1,19 +1,21 @@
 import { run } from '../../core'
 import { viewHandler } from '../../interfaces/view'
-
+import { warn, error } from '../../utils/log' // DEV ONLY
+import { mergeStates } from '../../utils/reattach' // DEV ONLY
 
 let app = run({
   root: require('./app').default,
   interfaces: {
     view: viewHandler('#app'),
-  }
+  },
+  warn,
+  error,
 })
 
-
-// Hot reload
+// Hot reload - DEV ONLY
 if (module.hot) {
   module.hot.accept('./app', () => {
     let m = require('./app').default
-    app.moduleAPI.reattach(m)
+    app.moduleAPI.reattach(m, mergeStates)
   })
 }
