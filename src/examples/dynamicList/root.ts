860d9118fd58e015bf46df17189a28e00a1b66f5
Carlos Galarza <carloslfu@gmail.com>
2017-04-03 00:08:32 -0500

    Add dynamic list example

---
+++ src/examples/dynamicList/root.ts
@@ -0,0 +1,140 @@
+import { Component, Actions, Inputs, ev, _ } from '../../core'
+import { action, act } from '../../utils/component'
+import { StyleGroup } from '../../utils/style'
+import { View } from '../../interfaces/view'
+import h from 'snabbdom/h'
+
+const name = 'Root'
+
+const state = {
+  text: '',
+  safeCount: 0,
+  list: {},
+}
+
+const inputs: Inputs = ctx => ({
+  action: action(actions),
+  inputKeyup: ([keyCode, text]) =>
+    keyCode === 13 && text !== ''
+    ? [
+      actions.SetText(''),
+      actions.New(text),
+    ] : actions.SetText(text),
+})
+
+const actions: Actions = {
+  SetText: text => s => {
+    s.text = text
+    return s
+  },
+  New: text => s => {
+    s.list[s.safeCount] = text
+    s.safeCount++
+    return s
+  },
+  Remove: idx => s => {
+    delete s.list[idx]
+    return s
+  },
+}
+
+const view: View = (ctx, s) => {
+  let style = ctx.groups.style
+
+  return h('div', {
+    key: ctx.name,
+    class: { [style.base]: true },
+  }, [
+    h('input', {
+      class: { [style.input]: true },
+      props: { value: s.text },
+      on: {
+        keyup: ev(ctx, 'inputKeyup', _, [
+          ['keyCode'],
+          ['target', 'value'],
+        ]),
+      },
+    }),
+    h('ul', {class: { [style.list]: true }},
+      Object.keys(s.list).map(
+        idx => h('li', {
+          key: idx,
+          class: { [style.item]: true },
+        }, [
+          <any> s.list[idx],
+          h('span', {
+            class: { [style.remove]: true },
+            on: {
+              click: act(ctx, ['Remove', idx]),
+            },
+          }, 'remove'),
+        ]),
+      )
+    ),
+  ])
+}
+
+const generalFont = {
+  fontFamily: 'sans-serif',
+  fontSize: '22px',
+  color: '#292828',
+}
+
+const style: StyleGroup = {
+  base: {
+    width: '100%',
+    height: '100%',
+    overflow: 'auto',
+    padding: '20px',
+    ...generalFont,
+  },
+  input: {
+    padding: '5px',
+    ...generalFont,
+    $nest: {
+      '&:focus': {
+        outline: '2px solid #13A513',
+      },
+    },
+  },
+  list: {
+    width: '400px',
+  },
+  item: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '10px 10px 10px 20px',
+    borderBottom: '1px solid #C1B8B8',
+  },
+  remove: {
+    fontSize: '20px',
+    padding: '3px',
+    borderRadius: '4px',
+    color: 'white',
+    backgroundColor: '#DB4343',
+    cursor: 'pointer',
+    userSelect: 'none',
+    $nest: {
+      '&:hover': {
+        backgroundColor: '#DE3030',
+      },
+    },
+  },
+}
+
+const comp: Component = {
+  name,
+  groups: {
+    style,
+  },
+  state,
+  inputs,
+  actions,
+  interfaces: {
+    view,
+  },
+}
+
+export default comp
+
+
