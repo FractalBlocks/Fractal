a387f94de602a795b5695fd5d9318c73e5827f40
Carlos Galarza <carloslfu@gmail.com>
2017-05-06 12:01:08 -0500

    Better Logging functions

--- src/log.ts
+++ src/log.ts
@@ -1,12 +1,39 @@
 
 // side effects for log functionality
 
-export const warn = (source: string, description: string) => console.warn(`source: ${source}, description: ${description}`)
+import { clone } from './core'
+import { stateOf } from './component'
 
-export const error = (source: string, description: string) => console.error(`source: ${source}, description: ${description}`)
+export const warn = (source: string, description: string) =>
+  console.warn(`source: ${source}, description: ${description}`)
 
+export const error = (source: string, description: string) =>
+  console.error(`source: ${source}, description: ${description}`)
+
+export const beforeInput = (ctx, inputName, data) => {
+  let state = clone(stateOf(ctx))
+  // <any> until groupCollapsed issue in TS repo is merged https://github.com/Microsoft/TypeScript/pull/15630
+  ;(<any> console.groupCollapsed)(
+    `%c input  %c${inputName} %cfrom %c${ctx.id}`,
+    'color: #626060; font-size: 12px;',
+    'color: #3b3a3a; font-size: 14px;',
+    'color: #3b3a3a; font-size: 12px;'
+  )
+  console.info('%c input data  ', 'color: rgb(9, 157, 225); font-weight: bold;', data)
+  console.info('%c prev state  ', 'color: #AFAFAF; font-weight: bold;', state)
+}
+
+// color for actions (not yet implemented) #58C6F8
+
+export const afterInput = (ctx, inputName, data) => {
+  let state = clone(stateOf(ctx))
+  console.info('%c next state  ', 'color: #3CA43F; font-weight: bold;', state)
+  console.groupEnd()
+}
 
 export const logFns = {
   warn,
   error,
+  beforeInput,
+  afterInput,
 }
