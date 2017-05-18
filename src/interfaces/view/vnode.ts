e5f33bb7bf3b744fb339e5443d6e122f29bf62ad
Carlos Galarza <carloslfu@gmail.com>
2017-05-18 09:05:47 -0500

    Include vnode into core view methods

--- src/interfaces/view/vnode.ts
+++ src/interfaces/view/vnode.ts
@@ -36,6 +36,14 @@ export interface VNodeData {
     [key: string]: any
 }
 
-export declare function vnode(sel: string | undefined, data: any | undefined, children: Array<VNode | string> | undefined, text: string | undefined, elm: Element | Text | undefined): VNode
+export function vnode(sel: string | undefined,
+                      data: any | undefined,
+                      children: Array<VNode | string> | undefined,
+                      text: string | undefined,
+                      elm: Element | Text | undefined): VNode {
+  let key = data === undefined ? undefined : data.key;
+  return {sel: sel, data: data, children: children,
+          text: text, elm: elm, key: key};
+}
 
 export default vnode
