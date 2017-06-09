201ad74329446c40713597c6df97edf28b871e72
Carlos Galarza <carloslfu@gmail.com>
2017-06-08 20:26:43 -0500

    Add isDescendant view helper

---
+++ src/interfaces/view/utils.ts
@@ -0,0 +1,11 @@
+
+export const isDescendant = (parent, child) => {
+  var node = child.parentNode
+  while (node != null) {
+    if (node == parent) {
+      return true
+    }
+    node = node.parentNode
+  }
+  return false
+}
