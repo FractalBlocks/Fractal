c32f28ea07bb466f482ee52ddd4056a322d06f47
Carlos Galarza <carloslfu@gmail.com>
2017-06-19 11:50:04 -0500

    Fix webpack dev config

--- config/webpack-dev.config.js
+++ config/webpack-dev.config.js
@@ -18,7 +18,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 /**
  * Webpack Constants
  */
-const ENV = process.env.ENV = process.env.NODE_ENV = 'develoment'
+const ENV = process.env.ENV = process.env.NODE_ENV = 'development'
 
 /* TODOs:
   - Option for run a single test
