7dd8030aa88b9b38fd08001b05279d40f1bdbb24
Carlos Galarza <carloslfu@gmail.com>
2017-06-22 12:14:26 -0500

    Hotfix for v2.9.1

--- src/groups/style.ts
+++ src/groups/style.ts
@@ -2,11 +2,12 @@ import { ModuleAPI } from '../core'
 import { createTypeStyle } from 'typestyle'
 import { TypeStyle } from 'typestyle/lib/internal/typestyle'
 import { styleGroup } from '../utils/style'
+
 // insert styles in a DOM container at head
 
 export const styleHandler = (containerName?: string, debug = false) => (mod: ModuleAPI) => {
   let container
-  if (containerName === undefined) {
+  if (containerName !== undefined) {
     container = document.createElement('style')
     // named container
     if (containerName !== '' && containerName !== undefined) {
