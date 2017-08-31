087221e1b36d9658c60230608a3b38cab1f96b85
Carlos Galarza <carloslfu@gmail.com>
2017-08-31 15:16:32 -0500

    Change types strategy

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,3 @@
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/webpack-env/index.d.ts" />
 /// <reference path="Mori.d.ts" />
+/// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="globals/webpack-env/index.d.ts" />
