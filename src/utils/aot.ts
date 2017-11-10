6cc8b2dd73099902f291575cc78c7768004a451e
Carlos Galarza <carloslfu@gmail.com>
2017-11-10 14:47:05 -0500

    Changes: - Implement htmlFn for replacing transformHTML function, allows customization - Implement transformHTML function in renderHTML - Implement base url for AOT / SSR

--- src/utils/aot.ts
+++ src/utils/aot.ts
@@ -1,74 +1,24 @@
 import fs = require('fs-jetpack')
-import { Module, Component, RunModule } from '../core'
-import { renderHTML } from './ssr'
+import { renderHTML, StaticRenderOptions } from './ssr'
 
 export interface PrerenderOptions {
-  root: Component<any>
-  runModule: RunModule
-  encoding?: string
   outputFile: string
-  htmlFile: string
   cssFile?: string
-  isStatic?: boolean // is isS this means there are no need of JS at all
-  bundlePaths?: string[],
-  url?: string, // canonical url
-  componentNames?: any, // will be merged client-side
-  title?: string
-  description?: string
-  keywords?: string
-  author?: string
-  extras?: string
-  lang?: string
-  version?: string
-  cb: { (app: Module) }
+  htmlFile: string
 }
 
-export async function prerender ({
-  root,
-  runModule,
-  encoding,
-  isStatic,
-  outputFile,
-  htmlFile,
-  cssFile,
-  lang,
-  version,
-  url,
-  title,
-  description,
-  keywords,
-  author,
-  extras,
-  bundlePaths,
-  componentNames,
-  cb,
-}: PrerenderOptions) {
+export async function prerender (preOp: PrerenderOptions, op: StaticRenderOptions) {
   try {
-    encoding = encoding || 'utf-8'
-    let html = fs.read(htmlFile, 'utf8')
-    let css = cssFile ? fs.read(cssFile, 'utf8') : ''
-    let htmlResult = await renderHTML({
-      root,
-      encoding,
-      runModule,
-      isStatic,
-      lang,
+    op.encoding = op.encoding || 'utf-8'
+    let html = fs.read(preOp.htmlFile, 'utf8')
+    let css = preOp.cssFile ? fs.read(preOp.cssFile, 'utf8') : ''
+    let htmlResult = await renderHTML(<any> {
+      ...op,
       html,
       css,
-      url,
-      version,
-      title,
-      description,
-      bundlePaths,
-      keywords,
-      author,
-      extras,
-      componentNames,
-      cb,
     })
-
-    fs.write(outputFile, htmlResult, { atomic: true })
-    console.log('Guardado en archivo ' + outputFile)
+    fs.write(preOp.outputFile, htmlResult, { atomic: true })
+    console.log('Guardado en archivo ' + preOp.outputFile)
   } catch (err) {
     throw err
   }
--- src/utils/ssr.ts
+++ src/utils/ssr.ts
@@ -1,14 +1,16 @@
 import { Component, RunModule, Module } from '../core'
 import _toHTML from '../toHTML'
 
+export const toHTML = _toHTML
+
 export interface StaticRenderOptions {
   root: Component<any>
   runModule: RunModule
   encoding?: string
-  html: string
+  html?: string
   css?: string
   bundlePaths?: string[],
-  url?: string, // canonical url
+  url?: string, // Canonical url
   componentNames?: any, // will be merged client-side
   title?: string
   description?: string
@@ -18,71 +20,75 @@ export interface StaticRenderOptions {
   lang?: string
   isStatic?: boolean // is isS this means there are no need of JS at all
   version?: string
-  cb: { (app: Module) }
+  base?: string // Base URL
+  htmlFn? (op: StaticRenderOptions, renderData: RenderData, app: Module) // Replace default template transform function
+  cb? (app: Module)
 }
 
-export const renderHTML = ({
-  root,
-  runModule,
-  encoding,
-  html,
-  css,
-  bundlePaths,
-  url,
-  componentNames,
-  title,
-  description,
-  keywords,
-  extras,
-  isStatic,
-  author,
-  lang,
-  version,
-  cb,
-}: StaticRenderOptions): Promise<string> => {
+export interface RenderData {
+  view: any
+  style: string
+}
+
+export const renderHTML = (op: StaticRenderOptions): Promise<string> => {
   return new Promise((resolve, reject) => {
     return (async () => {
       try {
-        var app = await runModule(root, false, { render: false })
-        await cb(app)
+        var app = await op.runModule(op.root, false, { render: false })
+        if (op.cb) await op.cb(app)
         let view = await app.rootCtx.components.Root.interfaces['view'](app.rootCtx.components.Root.state)
-        let styleStr = (css || '') + app.rootCtx.groupHandlers['style'].state.instance.getStyles()
-        html = html.replace('<!--##HTML##-->', _toHTML(view))
-        html = html.replace('<!--##STYLES##-->', '<style>' + styleStr + '</style>')
-        html = html.replace('<!--##ENCODING##-->', encoding || 'utf-8')
-        html = html.replace('<!--##DESCRIPTION##-->', description || '')
-        html = html.replace('<!--##KEYWORDS##-->', keywords || '')
-        html = html.replace('<!--##AUTHOR##-->', author || '')
-        html = html.replace('<!--##TITLE##-->', title || '')
-        html = html.replace('<!--##URL##-->', url || '/')
-        let bundles = bundlePaths.map(
-          p => `<script defer src="${p}?v=${version || ''}"></script>`
-        ).join('')
-        html = html.replace('<!--##BUNDLES##-->', bundles)
-        html = html.replace('<!--##EXTRAS##-->', extras || '')
-        html = html.replace('<!--##LANG##-->', lang || 'en')
-        html = html.replace('<!--##VERSION##-->', version || '')
-        let components = {}
-        let key
-        let subkey
-        for (key in app.rootCtx.components) {
-          if (componentNames && componentNames.indexOf(key) === -1) {
-            continue
-          }
-          components[key] = {}
-          for (subkey in app.rootCtx.components[key]) {
-            if (['state'].indexOf(subkey) !== -1) {
-              // avoid cyclic structure
-              components[key][subkey] = app.rootCtx.components[key][subkey]
-            }
-          }
+        let styleStr = (op.css || '') + app.rootCtx.groupHandlers['style'].state.instance.getStyles()
+        let renderData =  {
+          view,
+          style: styleStr,
         }
-        html = html.replace('<!--##COMPONENTS##-->', JSON.stringify(components))
-        html = html.replace('<!--##INITIALIZED##-->', 'true')
+        let html
+        if (op.htmlFn) {
+          html = await op.htmlFn(op, renderData, app)
+        } else {
+          html = await transformHTML(op, renderData, app)
+        }
         resolve(html)
       } catch (err) {
         reject(err)
       }
     })()
   })
+}
+
+export function transformHTML (op: StaticRenderOptions, renderData: RenderData, app: Module) {
+  let html = op.html.replace('<!--##HTML##-->', _toHTML(renderData.view))
+  html = html.replace('<!--##STYLES##-->', '<style>' + renderData.style + '</style>')
+  html = html.replace('<!--##ENCODING##-->', op.encoding || 'utf-8')
+  html = html.replace('<!--##DESCRIPTION##-->', op.description || '')
+  html = html.replace('<!--##KEYWORDS##-->', op.keywords || '')
+  html = html.replace('<!--##AUTHOR##-->', op.author || '')
+  html = html.replace('<!--##TITLE##-->', op.title || '')
+  html = html.replace('<!--##URL##-->', op.url || '/')
+  html = html.replace('<!--##BASE##-->', op.base || '/')
+  let bundles = op.bundlePaths.map(
+    p => `<script defer src="${p}?v=${op.version || ''}"></script>`
+  ).join('')
+  html = html.replace('<!--##BUNDLES##-->', bundles)
+  html = html.replace('<!--##EXTRAS##-->', op.extras || '')
+  html = html.replace('<!--##LANG##-->', op.lang || 'en')
+  html = html.replace('<!--##VERSION##-->', op.version || '')
+  let components = {}
+  let key
+  let subkey
+  for (key in app.rootCtx.components) {
+    if (op.componentNames && op.componentNames.indexOf(key) === -1) {
+      continue
+    }
+    components[key] = {}
+    for (subkey in app.rootCtx.components[key]) {
+      if (['state'].indexOf(subkey) !== -1) {
+        // avoid cyclic structure
+        components[key][subkey] = app.rootCtx.components[key][subkey]
+      }
+    }
+  }
+  html = html.replace('<!--##COMPONENTS##-->', JSON.stringify(components))
+  html = html.replace('<!--##INITIALIZED##-->', 'true')
+  return html
 }
