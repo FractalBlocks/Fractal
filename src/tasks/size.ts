ce4a6038780d7b9cd38ca88037c9f73134d03cf8
Carlos Galarza <carloslfu@gmail.com>
2017-12-05 21:42:09 -0500

    Fix size task internal type

--- src/tasks/size.ts
+++ src/tasks/size.ts
@@ -3,7 +3,7 @@ import { Handler } from '../core/handler'
 export const sizeHandler: Handler = () => mod => ({
   state: undefined,
   handle: async ([selector, prop, cb]) => {
-    let elements: HTMLElement[] = document.querySelectorAll(selector)
+    let elements = document.querySelectorAll(selector)
     let propValues = []
     for (let i = 0, len = elements.length; i < len; i++) {
       let element = elements[i]
