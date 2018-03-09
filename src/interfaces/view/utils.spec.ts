6088c05b1686669219f73d6b0d1eb98bccd8732d
Carlos Galarza <carloslfu@gmail.com>
2018-03-09 15:24:06 -0500

    Remove view utils spec temporarly

--- src/interfaces/view/utils.spec.ts
+++ src/interfaces/view/utils.spec.ts
@@ -1,25 +1,25 @@
 
-import { isDescendant } from './utils'
+// import { isDescendant } from './utils'
 
-describe('View helpers', () => {
+// describe('View helpers', () => {
 
-  describe('isDescendant evaluate if an element is descendant of another element', () => {
+//   describe('isDescendant evaluate if an element is descendant of another element', () => {
 
-    let ancestor = document.createElement('div')
-    let intermediate = document.createElement('div')
-    ancestor.appendChild(intermediate)
-    let descendant = document.createElement('div')
-    intermediate.appendChild(descendant)
-    let notDescendant = document.createElement('div')
+//     let ancestor = document.createElement('div')
+//     let intermediate = document.createElement('div')
+//     ancestor.appendChild(intermediate)
+//     let descendant = document.createElement('div')
+//     intermediate.appendChild(descendant)
+//     let notDescendant = document.createElement('div')
 
-    it('should return true if the element is decendant of the other', () => {
-      expect(isDescendant(ancestor, descendant)).toEqual(true)
-    })
+//     it('should return true if the element is decendant of the other', () => {
+//       expect(isDescendant(ancestor, descendant)).toEqual(true)
+//     })
 
-    it('should return false if the element is not decendant of the other', () => {
-      expect(isDescendant(ancestor, notDescendant)).toEqual(false)
-    })
+//     it('should return false if the element is not decendant of the other', () => {
+//       expect(isDescendant(ancestor, notDescendant)).toEqual(false)
+//     })
 
-  })
+//   })
 
-})
+// })
