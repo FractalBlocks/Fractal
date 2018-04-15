3b7bb546ee9a5a5c74fe58a7148a9e964fcb606d
Carlos Galarza <carloslfu@gmail.com>
2018-04-15 02:23:57 -0500

    Fix stateOf type signature

--- src/core/interface.ts
+++ src/core/interface.ts
@@ -14,7 +14,7 @@ import { _stateOf, _componentHelpers, CtxStateOf } from './input'
 export interface InterfaceHelpers<S> {
   ctx: Context<S>
   interfaceOf?: CtxInterfaceOf
-  stateOf: CtxStateOf<S>
+  stateOf: CtxStateOf
   in: CtxIn
   act: CtxAct
   vw?: CtxVw
